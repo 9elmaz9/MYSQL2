00b6a61ed04efb87628472b6536e43d47f1dec13
Elmaz <Kakbyelmaz@gmail.com>
2024-03-13 11:41:01 +0100

    Initial commit

---
+++ src/main/sql2/TheBelgianBreweryDB.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS TheBelgianBreweryDB;
+
+       USE TheBelgianBreweryDB;
+
+           SELECT
+               Id,Name,BrewerId,Category,Price,Stock,Alcohol,Version,Image
+
+           FROM Beers;
